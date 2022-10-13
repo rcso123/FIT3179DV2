37cc7fceaf8722284c7414385362e1f9194b244b
rcso123 <80513457+rcso123@users.noreply.github.com>
2022-10-14 06:28:54 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,17 @@
+var vg_1 = "daily_cost_bar.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+var vg_2 = "cpi_map.json";
+vegaEmbed("#map", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+var vg_2 = "cpi_map2.json";
+vegaEmbed("#map2", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
