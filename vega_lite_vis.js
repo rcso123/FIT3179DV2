c5140b7e64d3f78b62c01073596912ee4cc9a8c8
rcso123 <80513457+rcso123@users.noreply.github.com>
2022-10-17 18:11:25 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,29 @@
+var vg_1 = "daily_cost_bar.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+var vg_2 = "cpi_map.json";
+vegaEmbed("#map", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+var vg_2 = "cpi_map2.json";
+vegaEmbed("#map2", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+var vg_2 = "scatterplot.json";
+vegaEmbed("#scatterplot", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+
+// var vg_2 = "alt_map.json";
+// vegaEmbed("#map2", vg_2).then(function(result) {
+// // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
